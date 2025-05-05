fdeb6cafab3bbf792e9aa689b48c8fc652d32764
Otávio Vínicios Borges <otavio.vinicios.borges@gmail.com>
2025-05-05 18:01:19 -0300

    Create constants.py

---
+++ month001/week002/day012/constants.py
@@ -0,0 +1,17 @@
+# Constants for various scientific and mathematical calculations
+# Constants are defined in a separate file for better organization and reusability
+# Constants are typically written in uppercase letters
+# Constants can be changed in the future if needed, but it's a good practice to keep them consistent
+PI = 3.14159265358979323846
+E = 2.71828182845904523536
+GOLDEN_RATIO = 1.618033988749895
+SPEED_OF_LIGHT = 299792458  # in meters per second
+GRAVITATIONAL_CONSTANT = 9.80665  # in m/s^2
+AVOGADRO_NUMBER = 6.02214076e23  # in mol^-1
+BOLTZMANN_CONSTANT = 1.380649e-23  # in J/K
+PLANCK_CONSTANT = 6.62607015e-34  # in J*s
+SPEED_OF_SOUND = 343  # in m/s at 20 degrees Celsius
+MOLECULAR_WEIGHT_WATER = 18.01528  # in g/mol
+MOLECULAR_WEIGHT_OXYGEN = 15.999  # in g/mol
+MOLECULAR_WEIGHT_CARBON_DIOXIDE = 44.01  # in g/mol
+print(PI)
