e530e5e1a53cad6970dc34c08630b2c19b6e0ba2
Otávio Vínicios Borges <otavio.vinicios.borges@gmail.com>
2025-05-09 19:52:41 -0300

    Day 14 completed

---
+++ apps/followers/game_data.py
@@ -0,0 +1,26 @@
+data = [
+    {
+        'name': 'Instagram',
+        'followers': 600000000,
+        'description': 'Instagram is a photo and video sharing social networking service owned by Facebook, Inc. It allows users to upload media that can be edited with filters and organized by hashtags and geographical tagging.',
+        'country': 'United States'
+    },
+    {
+        'name': 'Facebook',
+        'followers': 2900000000,
+        'description': 'Facebook is a social media platform that allows users to connect with friends and family, share content, and discover news and information.',
+        'country': 'United States'
+    },
+    {
+        'name': 'Twitter',
+        'followers': 330000000,
+        'description': 'Twitter is a microblogging and social networking service where users post and interact with messages known as "tweets".',
+        'country': 'United States'
+    },
+    {
+        'name': 'TikTok',
+        'followers': 1000000000,
+        'description': 'TikTok is a social media platform for creating, sharing, and discovering short music videos.',
+        'country': 'China'
+    }
+]
