495b23b5f0de3b15b68a4a8e6d6d179d6139679b
Otávio Vínicios Borges <otavio.vinicios.borges@gmail.com>
2025-05-15 14:01:34 -0300

    Erased apps

--- apps/band_name_gerenator.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("Band Name Generator.")
-    city = input("What is the name of the city you grew up in?\n")
-    pet = input("What is the name of your pet?\n")
-    print(f"Your Band name could be {city} {pet}")
-
-
-if __name__ == "__main__":
-    main()
